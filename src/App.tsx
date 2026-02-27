e06a626acef726c51814c62aead8dc406484d849
Mohanraj M <126415700+MaX-NeO@users.noreply.github.com>
2026-02-27 14:40:06 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen w-screen flex items-center justify-center p-0 m-0 bg-amber-100/30 overflow-hidden">
+        <div className=' h-[30rem] w-[30rem] bg-white rounded-lg shadow-lg shadow-purple-500/30 flex items-center justify-center text-2xl font-bold  flex-col gap-2'>
+          <h1 className='text-purple-500 font-bold text-2xl'>Name</h1>
+          <h5 className='text-gray-500'>rollnumber</h5>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
